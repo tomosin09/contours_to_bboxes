86463bdd9ec1e1ef9da4796b393789231c7746ca
tomosin09 <mikotomisaki@gmail.com>
2021-08-17 20:24:46 +0300

    [ADD] generate txt file with train data

--- get_bbox.py
+++ get_bbox.py
@@ -57,8 +57,6 @@ def draw_landmarks(path_to_images, landmarks, len_list):
     titles = []
     len_list = len_list if len(path_to_images) > len_list else len(path_to_images)
     for i in range(len_list):
-        print(path_to_images[i])
-        print(landmarks[i])
         image = cv2.imread(path_to_images[i])
         title = i
         landmark = np.array(landmarks[i])
@@ -78,7 +76,10 @@ def draw_bbox(path_to_images, bboxes, len_list):
         image = cv2.imread(path_to_images[i])
         title = i
         for bbox in bboxes[i]:
-            cv2.rectangle(image, (int(bbox[0][0]), int(bbox[0][1])), (int(bbox[1][0]), int(bbox[1][1])), (0, 255, 0), 3)
+            start_point = (int(bbox[0][0]), int(bbox[0][1]))
+            end_point = bbox[0][0] + bbox[1], bbox[0][1]+bbox[2]
+            cv2.rectangle(image, start_point,
+                          end_point, (0, 255, 0), 3)
         images.append(image)
         titles.append(title)
     return images, titles
@@ -90,12 +91,46 @@ def get_bbox_list(landmarks):
         bbox = []
         landmark = np.array(i)
         for contour in landmark:
-            point_1 = np.min(contour, axis=0)
-            point_2 = np.max(contour, axis=0)
-            pts = [point_1.tolist(), point_2.tolist()]
+            start_point = np.min(contour, axis=0)
+            end_point = np.max(contour, axis=0)
+            width = end_point[0] - start_point[0]
+            height = end_point[1] - start_point[1]
+            pts = [start_point.tolist(), width, height]
             bbox.append(pts)
         bboxes.append(bbox)
     return bboxes
+
+def get_data_wider_face(filenames, bboxes, landmarks):
+    wider_data = {'names':[], 'data':[]}
+    if len(filenames) == len(bboxes) and len(filenames) == len(landmarks):
+        print('------------------------------\n'
+              'lists names, objects and landmargs is equal'
+              '\n------------------------------')
+        for i in range(len(filenames)):
+            wider_data['names'].append(filenames[i])
+            data = []
+            for b, l in zip(bboxes[i], landmarks[i]):
+                data.extend([[b[0][0], b[0][1], b[1], b[2],
+                             l[0][0], l[0][1], l[1][0],
+                             l[1][1], l[2][0], l[2][1], l[3][0], l[3][1]]])
+            wider_data['data'].append(data)
+        print(f'wider_name is {wider_data["names"][:5]}\n'
+              f'and data {wider_data["data"][:5]}')
+        return wider_data
+    else:
+        print('------------------------------\n'
+              'lists names, objects and landmargs is not equal, make sure the data has not been lost'
+              '\n------------------------------')
+        return None
+
+def create_wider_label(names, points):
+    f = open('label.txt', 'w')
+    for name, pts in zip(names, points):
+        f.write(f'# {name}\n')
+        for pt in pts:
+            for val in pt:
+                f.write(f'{val} ')
+            f.write('\n')
 
 
 if __name__ == "__main__":
@@ -109,5 +144,16 @@ if __name__ == "__main__":
     # to_show(images, titles)
 
     bboxes = get_bbox_list(objects['landmarks'])
+
+    # Check correct bboxes
     # images_bbox, titles_bbox = draw_bbox(path_to_images, bboxes, 10)
     # to_show(images_bbox, titles_bbox)
+
+    objects['bboxes'] = bboxes
+    wider_data = get_data_wider_face(objects['filename'], objects['bboxes'], objects['landmarks'])
+
+    create_wider_label(wider_data['names'], wider_data['data'])
+
+
+
+
